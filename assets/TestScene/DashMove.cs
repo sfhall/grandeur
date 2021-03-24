3c8dad3024aa5e3dca852df83adc62603b5c39e3
JustWiII <jw1116@live.com>
2021-03-24 13:29:26 -0400

    Removed old character assets
    Removed old character assets and added the new character sprite in the TestScene folder. New character sprite has new movement scripts and new dash function.

---
+++ assets/TestScene/DashMove.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashMove : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+    private Rigidbody2D rb;
+    public float dashSpeed;
+    private float dashTime;
+    public float startDashTime;
+    private int direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        dashTime = startDashTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(direction == 0)
+        {
+            if(Input.GetKeyDown(KeyCode.F))
+            {
+                direction = 1;
+                animator.SetInteger("IsDashing", direction);
+            }
+        }
+        else
+        {
+            if(dashTime <= 0)
+            {
+                direction = 0;
+                dashTime = startDashTime;
+                rb.velocity = Vector2.zero;
+                animator.SetInteger("IsDashing", direction);
+            }
+            else
+            {
+                dashTime -= Time.deltaTime;
+
+                if (controller.m_FacingRight == true)
+                {
+                    rb.velocity = Vector2.right * dashSpeed;
+                }
+                else if (controller.m_FacingRight == false)
+                {
+                    rb.velocity = Vector2.left * dashSpeed;
+                }
+
+                /*if(direction == 1)
+                {
+                    rb.velocity = Vector2.left * dashSpeed;
+                }
+                else if(direction == 2)
+                {
+                    rb.velocity = Vector2.right * dashSpeed;
+                }*/
+            }
+        }
+    }
+}
