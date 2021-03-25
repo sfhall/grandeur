4f5b7cefeec4e2768697c2d0e6a86893f5787486
Sarah Hall <sfhall22@gmail.com>
2021-03-25 11:49:25 -0400

    scene change works

--- assets/Scripts/PlayerMovement.cs
+++ assets/Scripts/PlayerMovement.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
-
     public Animator animator;
+
+    public GameObject[] doors;
+    public GameObject player;
+    //this value is use to allow wiggle room for entering doors
+    public float give;
 
     float horizontalMove = 0f;
 
@@ -36,6 +41,10 @@ public class PlayerMovement : MonoBehaviour
         {
             crouch = false;
         }
+        else if(Input.GetButtonDown("up"))
+        {
+            LoadLevel();
+        }
     }
 
     public void OnLanding()
@@ -54,5 +63,20 @@ public class PlayerMovement : MonoBehaviour
         // Move our character
         controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
         jump = false;
+    }
+
+    private void LoadLevel()
+    {
+        //check if in range of door and load approprate scene
+        string[] doorNames = {"Level1", "Level2", "Level3", "Level4", "Level5", "Level6", "Level7", "Level8"};
+        for(int i = 0; i < 8; i++)
+        {
+            //Debug.Log(doors[i].transform.localPosition);
+            //subtract 10 for adjustment
+            float compare = Mathf.Abs(player.transform.position.x - doors[i].transform.position.x);
+            if(compare < give && compare > 0)
+                SceneManager.LoadScene(doorNames[i]);
+        }
+        return;
     }
 }
