d6d3aef26a3abc609ba4f6c751459ff8a95c065b
Sarah Hall <sfhall22@gmail.com>
2021-04-22 13:13:35 -0400

    fixed corner case for openDoor script

--- assets/Scripts/OpenDoor.cs
+++ assets/Scripts/OpenDoor.cs
@@ -10,15 +10,21 @@ public class OpenDoor : MonoBehaviour
     public GameObject door;
     public GameObject player;
 
+    public bool open;
+
     public float give;
 
-
+    void Start()
+    {
+        open = false;
+    }
     // Update is called once per frame
     void Update()
     {
         if(key == null)
         {
             grate.SetActive(false);
+            open = true;
         }
         
         if(Input.GetButtonDown("Enter"))
@@ -32,7 +38,7 @@ public class OpenDoor : MonoBehaviour
         float compare = Mathf.Abs(player.transform.position.x - door.transform.position.x);
         Debug.Log(compare);
 
-        if(compare < give && compare > 0)
+        if(compare < give && compare > 0 && open)
             SceneManager.LoadScene("Level0");
     }
 }
